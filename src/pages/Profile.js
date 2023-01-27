e5460b22423d2d4133825dc66f505b5c8dbd800a
Samara Bastos <samarasampaio30@gmail.com>
2023-01-27 11:11:55 -0300

    Requisitos 57 ao 62 - Profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+  const email = user ? user.email : null;
+
   return (
     <div>
       <Header
         title="Profile"
         haveSearch={ false }
       />
-      <Footer />
+      <div>
+        <div>
+          <h3 data-testid="profile-email">{email}</h3>
+          <Link
+            data-testid="profile-done-btn"
+            to="/done-recipes"
+          >
+            Done Recipes
+          </Link>
+          <Link
+            data-testid="profile-favorite-btn"
+            to="/favorite-recipes"
+          >
+            Favorite Recipes
+          </Link>
+          <Link
+            data-testid="profile-logout-btn"
+            onClick={ () => localStorage.clear() }
+            to="/"
+          >
+            Logout
+          </Link>
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
