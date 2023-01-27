cd88ceee71e52d98784b6e20a7bedb6f24919dbc
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-27 02:13:19 -0300

    req: 24 -- feito

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import { getMealsByID, getDrinksByID } from '../services/api';
 
 function RecipeDetails() {
+  const location = useLocation();
+  const [recipe, setRecipe] = useState([]);
+
+  useEffect(() => {
+    const type = location.pathname.split('/')[1];
+    const id = location.pathname.split('/')[2];
+    if (type === 'meals') {
+      getMealsByID(id).then((data) => setRecipe(data));
+    } else {
+      getDrinksByID(id).then((data) => setRecipe(data));
+    }
+  }, [location.pathname]);
+  console.log(recipe);
   return (
     <div>RecipeDetails</div>
   );
