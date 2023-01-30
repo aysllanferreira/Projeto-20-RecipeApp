17d829517852357a099b0ccdf4e6fe7b46a3e234
Aysllan Ferreira <aysllan.ferreiraks@gmail.com>
2023-01-29 22:28:09 -0300

    req: 44- feito

--- src/pages/DoneRecipes.js
+++ src/pages/DoneRecipes.js
@@ -1,13 +1,72 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
 function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    const doneRecipesStorage = JSON.parse(localStorage.getItem('doneRecipes'));
+    setDoneRecipes(doneRecipesStorage);
+  }, []);
+  console.log(doneRecipes);
   return (
     <div>
       <Header
         title="Done Recipes"
         haveSearch={ false }
       />
+
+      <button
+        type="button"
+        data-testid="filter-by-all-btn"
+      >
+        All
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-meal-btn"
+      >
+        Food
+      </button>
+      <button
+        type="button"
+        data-testid="filter-by-drink-btn"
+      >
+        Drinks
+      </button>
+
+      {doneRecipes && doneRecipes.map((recipe, index) => (
+        <div key={ recipe.id }>
+          <img
+            src={ recipe.image }
+            alt={ recipe.name }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <p data-testid={ `${index}-horizontal-top-text` }>
+            {recipe.type === 'meal'
+              ? `${recipe.area} - ${recipe.category}` : recipe.alcoholicOrNot}
+          </p>
+          <p data-testid={ `${index}-horizontal-name` }>{recipe.name}</p>
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          <button
+            type="button"
+            data-testid={ `${index}-horizontal-share-btn` }
+          >
+            <img
+              src="https://img.icons8.com/ios/50/000000/share--v1.png"
+              alt="share"
+            />
+          </button>
+          {recipe.tags && recipe.tags.map((tag, indexTag) => (
+            <p
+              key={ tag }
+              data-testid={ `${index}-${tag}-horizontal-tag` }
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
